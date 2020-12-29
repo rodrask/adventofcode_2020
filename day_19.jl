8ca70ead7d458365b9b76d2b72faa77a5bb3db68
Pavel Okunev <p.okunev@corp.mail.ru>
2020-12-29 12:55:16 +0300

    day 19 solution

--- day_19.jl
+++ day_19.jl
@@ -3,7 +3,6 @@ using StringParserPEG
 function build_grammar(file)
     grammar = []
     while (line = readline(file)) != ""
-        line = replace(line, r"^0:" => "start =>")
         line = replace(line, r"(\:)" => " =>")
         line = replace(line, r"\"" => "'")
         line = replace(line, r"(\d+) (\d+) (\d+) (\d+)" => s"\1 & \2 & \3 & \4")
@@ -12,21 +11,24 @@ function build_grammar(file)
         result = replace(line, r"(\d+)" => s"rule_\1")
         push!(grammar, result)
     end
+    println(join(grammar,"\n"))
     Grammar(join(grammar,"\n"))
 end
 
 function main()
-    grammar,test_lines= open("day_19.txt", "r") do file
+    grammar,test_lines= open("day_19_test.txt", "r") do file
         build_grammar(file), readlines(file)
     end
+    show(grammar)
     sum = 0
     for test in test_lines
-        (ast, pos, err) = parse(grammar, test)
+        (ast, pos, err) = parse(grammar, test;start=:rule_0)
         if err === nothing
             sum += 1
+        else
+            @show err
         end
     end
-    @show sum
     println(sum)
 end
 
